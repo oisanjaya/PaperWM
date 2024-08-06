ef096f862b24c7e3adf83ded467b0dc885f4f02d
Jay Ta'ala <jay@jaytaala.com>
2024-08-06 22:45:43 +1000

    FIX: onComplete function optional chaining guard.

--- scratch.js
+++ scratch.js
@@ -42,10 +42,11 @@ export function disable() {
    other windows/clones (clones if the space animates)
  */
 export function easeScratch(metaWindow, targetX, targetY, params = {}) {
-    let f = metaWindow.get_frame_rect();
-    let b = metaWindow.get_buffer_rect();
-    let dx = f.x - b.x;
-    let dy = f.y - b.y;
+    const complete = params?.onComplete ?? function() {};
+    const f = metaWindow.get_frame_rect();
+    const b = metaWindow.get_buffer_rect();
+    const dx = f.x - b.x;
+    const dy = f.y - b.y;
 
     Easer.addEase(metaWindow.get_compositor_private(), {
         x: targetX - dx,
@@ -53,7 +54,7 @@ export function easeScratch(metaWindow, targetX, targetY, params = {}) {
         time: Settings.prefs.animation_time,
         onComplete: () => {
             metaWindow.move_frame(true, targetX, targetY);
-            params?.onComplete();
+            complete();
         },
     });
 }
